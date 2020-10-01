ed6432929bf0647ca874aa6be02caa42f5425427
jess-izuu <izumijessica@gmail.com>
2020-10-01 17:48:01 -0400

    superhero movie grid cell and view controller

---
+++ Flixster_iOS/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster_iOS
+//
+//  Created by Jessica Izumi on 10/1/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
